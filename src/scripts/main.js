ee2bef58e1944ebd85a7e465e0e5c9c9107e186d
Corri Golden <corri.golden@gmail.com>
2019-10-16 14:59:52 -0500

    Finished Represent Pet

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+console.log("Dale! ponele media pilas")
+
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+myPet = {
+    name: "Dori",
+    species: "cat",
+    nicknames: ["dorita", "dd", "socks"],
+    age: 7,
+}
+
+console.log(myPet)
